cfb0cf0e16c8135e922b1d800d36460a4e4141bc
mtrazzak <muhammed.razzak@gmail.com>
2023-06-29 02:52:19 +0100

    push to repo

--- README.md
+++ README.md
@@ -1,2 +1,163 @@
-# lightning-mae
-A Simple PyTorch Lightning implementation of Masked Autoencoder
+# Masked Autoencoder in PyTorch Lightning
+![PyTorch](https://img.shields.io/badge/pytorch-2.0.0-green) [![arXiv](https://img.shields.io/badge/arXiv-2111.06377-b31b1b.svg)](https://arxiv.org/abs/2111.06377)
+
+This repository provides an implementation of the Masked Autoencoder (MAE) framework, a deep learning model for unsupervised representation learning. The MAE model is designed to reconstruct input data from partially masked versions, allowing it to learn meaningful representations that capture important features and patterns in the data. 
+
+## Table of Contents
+- [Overview](#overview)
+- [Installation](#installation)
+- [Masked Autocoders](#masked-autoencoders)
+- [Training the Masked Autoencoder (MAE) Model](#training-the-masked-autoencoder-mae-model)
+- [Converting the MAE Model to a Vision Transformer (ViT)](#converting-the-mae-model-to-a-vision-transformer-vit)
+
+
+## Overview
+
+The Masked Autoencoders (MAE) training framework uses an approach where random patches of the input image are masked and the missing pixels are reconstructed. This approach is based on two core designs: an asymmetric encoder-decoder architecture and masking a high proportion of the input image.
+
+1. **Asymmetric encoder-decoder architecture:** The encoder operates only on the visible subset of patches (without mask tokens), and a lightweight decoder reconstructs the original image from the latent representation and mask tokens. This design allows for efficient and effective training of large models.
+
+2. **High proportion of input image masking:** Masking around 75% of the input image creates a nontrivial and meaningful self-supervisory task. This encourages learning useful features and reduces redundancy, creating a challenging self-supervisory task that requires a holistic understanding beyond low-level image statistics.
+
+The MAE method has been found to be scalable and effective for visual representation learning, achieving high accuracy and improved generalization performance when used for pre-training data-hungry models like ViT-Large/-Huge on ImageNet-1K. Transfer learning performance in downstream tasks also outperforms supervised pre-training and shows promising scaling behavior.
+
+The original GitHub repository for the project can be found at [this link](https://github.com/facebookresearch/MAE).
+
+The citation for the original paper "Masked Autoencoders Are Scalable Vision Learners" by Kaiming He et al. is as follows:
+
+```bibtex
+@article{MaskedAutoencoders2021,
+  author  = {Kaiming He and Xinlei Chen and Saining Xie and Yanghao Li and Piotr Doll{\'a}r and Ross Girshick},
+  journal = {arXiv:2111.06377},
+  title   = {Masked Autoencoders Are Scalable Vision Learners},
+  year    = {2021},
+}
+```
+
+For more in-depth information, please refer to the [original paper](https://arxiv.org/abs/2111.06377) and [GitHub repository](https://github.com/facebookresearch/MAE).
+
+## Installation
+
+To use the MAE framework, follow these steps:
+
+1. Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/username/repo.git
+```
+
+2. Install the required dependencies by running the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+This command will install the necessary packages, including PyTorch, torchvision, timm, and transformers.
+
+Certainly! Here's an updated version of the ReadMe file with improved headings, additional details on the MAE framework, and clearer instructions:
+
+## Masked Autoencoders
+
+This repository provides an implementation of the Masked Autoencoder (MAE) framework, a deep learning model for unsupervised representation learning. The MAE model is designed to reconstruct input data from partially masked versions, allowing it to learn meaningful representations that capture important features and patterns in the data. This ReadMe guide provides instructions on installing the repository and training the MAE model.
+
+## Installation
+
+To use the MAE framework, follow these steps:
+
+1. Clone this repository to your local machine:
+
+   ```
+   git clone https://github.com/mtrazzak/lightning-mae.git
+   ```
+
+2. Install the required dependencies by running the following command:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+   This command will install the necessary packages, including PyTorch, torchvision, timm, and transformers.
+
+## Training the Masked Autoencoder (MAE) Model
+
+To train the MAE model with the default parameters and masking ratio, follow these steps:
+
+1. Prepare your training data: The MAE model expects input data in tensor format. You can preprocess your dataset and convert it into tensors using appropriate techniques and libraries.
+
+2. Import the `MaskedAutoencoderLIT` class from the `masked_autoencoder` module:
+
+   ```python
+   from mae import MaskedAutoencoder
+   ```
+
+3. Initialize the MAE model with the desired parameters. By default, the model uses the 'base' size, 12 input channels, a base learning rate of 3e-5, 4 GPUs, a batch size of 512, 1 warm-up epoch, a weight decay of 0.05, and betas of (0.9, 0.95):
+
+   ```python
+   model = MaskedAutoencoder()
+   ```
+
+4. Prepare your training data as PyTorch datasets and data loaders. You can utilize `torchvision.datasets` and `torch.utils.data.DataLoader` to load and batch your data:
+
+   ```python
+   import torchvision.transforms as transforms
+   from torch.utils.data import DataLoader
+
+   transform = transforms.Compose([
+       transforms.ToTensor(),
+       # Add any additional transformations if needed
+   ])
+
+   train_dataset = torchvision.datasets.YourDataset(root='./data', train=True, transform=transform)
+   train_dataloader = DataLoader(train_dataset, batch_size=512, shuffle=True)
+   ```
+
+5. Create a Lightning `Trainer` object and initiate the training process using the `fit` method:
+
+   ```python
+   from pytorch_lightning import Trainer
+
+   trainer = Trainer(max_epochs=10, gpus=1)
+   trainer.fit(model, train_dataloader)
+   ```
+
+   This will train the MAE model for 10 epochs using the provided training data.
+
+6. After training, save the trained model's state dictionary using `torch.save`:
+
+   ```python
+   torch.save(model.state_dict(), 'masked_autoencoder.pt')
+   ```
+
+   The trained model can be loaded later for inference or fine-tuning.
+
+## Converting the MAE Model to a Vision Transformer (ViT)
+
+To convert the trained MAE model to a Vision Transformer (ViT) model for fine-tuning, follow these additional steps:
+
+1. Import the `get_vit_from_mae` function from the `conversion_utils` module:
+
+   ```python
+   from mae_to_vit import get_vit_from_mae
+   ```
+
+2. Load the trained MAE model using the `MaskedAutoencoder.load_from_checkpoint` method:
+
+   ```python
+   trained_model =
+
+ MaskedAutoencoder.load_from_checkpoint('masked_autoencoder.pt')
+   ```
+
+3. Convert the trained MAE model to a ViT model using the `get_vit_from_mae` function:
+
+   ```python
+   vit_model = get_vit_from_mae(trained_model.model.state_dict())
+   ```
+
+   The `get_vit_from_mae` function takes the pretrained MAE model state dictionary as input and returns a ViT model for fine-tuning. The `vit_model` can be used for further training or downstream tasks.
+
+Please note that you may need to adjust the code and parameters according to your specific use case and dataset.
+
+For more detailed information on the MAE framework, ViT conversion, and customization options, please refer to the code documentation and the source files in this repository.
+
+Happy training!
---
+++ mae.py
@@ -0,0 +1,469 @@
+# Built-in Libraries
+from functools import partial
+
+# External Libraries
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import wandb
+from lightning import LightningModule
+from timm.models.vision_transformer import Block, PatchEmbed
+from transformers import get_cosine_schedule_with_warmup
+
+# typing
+from typing import Optional, Tuple
+
+class MaskedAutoencoderLIT(LightningModule):
+    def __init__(
+        self,
+        size: str = 'base',
+        in_chans: int = 12,
+        base_lr: float = 3e-5,
+        num_gpus: int = 4,
+        batch_size: int = 512,
+        warmup_epochs: int = 1,
+        weight_decay: float = 0.05,
+        betas: Tuple[float, float] = (0.9, 0.95)
+    ):
+        super().__init__()
+        self.size = size
+        self.in_chans = in_chans
+        self.base_lr = base_lr
+        self.num_gpus = num_gpus
+        self.batch_size = batch_size
+        self.warmup_epochs = warmup_epochs
+        self.weight_decay = weight_decay
+        self.betas = betas
+
+        # Initialize the model based on the specified size
+        if self.size == 'base':
+            self.model = mae_vit_base_patch16_dec512d8b(in_chans=in_chans)
+        elif self.size == 'large':
+            self.model = mae_vit_large_patch16_dec512d8b(in_chans=in_chans)
+        elif self.size == 'huge':
+            self.model = mae_vit_huge_patch14_dec512d8b(in_chans=in_chans)
+            
+        self.automatic_optimization = False
+
+    def forward(self, x: torch.Tensor, mask_ratio: float = 0.75) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Forward pass of the model.
+
+        Args:
+            x: Input tensor
+            mask_ratio: Ratio of masking applied to the input
+
+        Returns:
+            Tuple containing the loss, predicted output, and mask
+        """
+        loss, pred, mask = self.model(x, mask_ratio=mask_ratio)
+        return loss, pred, mask
+
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
+        """
+        Training step of the model.
+
+        Args:
+            batch: Tuple containing the input batch and labels
+            batch_idx: Index of the current batch
+
+        Returns:
+            Loss value
+        """
+        # Do optimization with optimizer and scheduler
+        opt = self.optimizers()
+        opt.zero_grad()
+
+        x, _ = batch
+        loss, _, _ = self(x)
+
+        self.manual_backward(loss)
+        opt.step()
+        sch = self.lr_schedulers()
+        sch.step()
+
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+        """
+        Validation step of the model.
+
+        Args:
+            batch: Tuple containing the input batch and labels
+            batch_idx: Index of the current batch
+        """
+        x, _ = batch
+        loss, pred, mask = self(x)
+        
+        # Create grid using torchvision of images, predicted and mask
+        if batch_idx == 0:
+            full_grid = self.generate_grid_of_samples(x, pred, mask)
+            self.logger.experiment.log({"sample_images": [wandb.Image(full_grid, caption="Predicted Images")]})
+        
+        self.log('val_loss', loss, on_step=True, on_epoch=True, sync_dist=True)
+
+    def configure_optimizers(self):
+        self.lr = self.base_lr * self.num_gpus * self.batch_size / 256
+        epoch_steps = self.trainer.estimated_stepping_batches // self.trainer.max_epochs
+        num_warmup_steps = int(epoch_steps * self.warmup_epochs)
+        
+        # Define the optimizer and scheduler
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay, betas=self.betas)
+        
+        scheduler = get_cosine_schedule_with_warmup(
+                optimizer,
+                num_training_steps=(self.trainer.estimated_stepping_batches),
+                num_warmup_steps=num_warmup_steps,
+            )
+        
+        return [optimizer], [scheduler]
+
+    def generate_grid_of_samples(self, true_images: torch.Tensor, pred_patch: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """
+        Generate a grid of samples for visualization.
+
+        Args:
+            true_images: Tensor containing the true images
+            pred_patch: Tensor containing the predicted patches
+            mask: Tensor containing the mask
+
+        Returns:
+            Tensor representing the grid of samples
+        """
+        x_patch = self.model.patchify(true_images)
+        # Merge original and predicted patches
+        pred_patch = pred_patch * mask[:, :, None]
+        x_patch = x_patch * (~mask[:, :, None].bool()).float()
+        res = self.model.unpatchify(x_patch) + self.model.unpatchify(pred_patch)
+        # Select three random samples from the dataset
+        sample_indices = torch.randperm(len(true_images))[:3]
+        
+        # Get the RGB bands for each sample
+        pred_images = res[sample_indices][:, [3, 2, 1]]
+        true_image_grid = true_images[sample_indices][:, [3, 2, 1]]
+
+        grid_images = torch.cat([true_image_grid, pred_images], dim=0)
+
+        # Create a grid for displaying the images
+        full_grid = torchvision.utils.make_grid(grid_images, nrow=3)
+        # Convert the grid to a numpy array and transpose the dimensions to (height, width, channels)
+        full_grid = full_grid.permute(1, 2, 0).float().cpu().numpy()
+        
+        # Log the image to Weights and Biases
+        return full_grid
+
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+class MaskedAutoencoderViT(nn.Module):
+    """ Masked Autoencoder with VisionTransformer backbone
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.in_chans = in_chans
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * in_chans, bias=True) # decoder to patch
+        # --------------------------------------------------------------------------
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def patchify(self, imgs):
+        """
+        imgs: (N, C, H, W)
+        x: (N, L, patch_size**2 * C)
+        """
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], self.in_chans, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * self.in_chans))
+        return x
+
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 * C)
+        imgs: (N, C, H, W)
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1]**.5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, self.in_chans))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], self.in_chans, h * p, h * p))
+        return imgs
+
+    def random_masking(self, x, mask_ratio):
+        """
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
+        """
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+        
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+        
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+
+    def forward_encoder(self, x, mask_ratio):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        x, mask, ids_restore = self.random_masking(x, mask_ratio)
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask, ids_restore
+
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
+        """
+        imgs: [N, 3, H, W]
+        pred: [N, L, p*p*3]
+        mask: [N, L], 0 is keep, 1 is remove, 
+        """
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
+            mean = target.mean(dim=-1, keepdim=True)
+            var = target.var(dim=-1, keepdim=True)
+            target = (target - mean) / (var + 1.e-6)**.5
+
+        loss = (pred - target) ** 2
+        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        return loss
+
+    def forward(self, imgs, mask_ratio=0.75):
+        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
+        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+        loss = self.forward_loss(imgs, pred, mask)
+        return loss, pred, mask
+
+def mae_vit_base_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), norm_pix_loss=False,**kwargs)
+    return model
+
+def mae_vit_large_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=nn.LayerNorm, **kwargs)
+    return model
+
+def mae_vit_huge_patch14_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=14, embed_dim=1280, depth=32, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+# --------------------------------------------------------
+# 2D sine-cosine position embedding
+# References:
+# Transformer: https://github.com/tensorflow/models/blob/master/official/nlp/transformer/model_utils.py
+# MoCo v3: https://github.com/facebookresearch/moco-v3
+# --------------------------------------------------------
+def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    grid_h = np.arange(grid_size, dtype=float)
+    grid_w = np.arange(grid_size, dtype=float)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_size, grid_size])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+    if cls_token:
+        pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1) # (H*W, D)
+    return emb
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=float)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out) # (M, D/2)
+    emb_cos = np.cos(out) # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+# --------------------------------------------------------
+# Interpolate position embeddings for high-resolution
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+def interpolate_pos_embed(model, checkpoint_model):
+    if 'pos_embed' in checkpoint_model:
+        pos_embed_checkpoint = checkpoint_model['pos_embed']
+        embedding_size = pos_embed_checkpoint.shape[-1]
+        num_patches = model.patch_embed.num_patches
+        num_extra_tokens = model.pos_embed.shape[-2] - num_patches
+        # height (== width) for the checkpoint position embedding
+        orig_size = int((pos_embed_checkpoint.shape[-2] - num_extra_tokens) ** 0.5)
+        # height (== width) for the new position embedding
+        new_size = int(num_patches ** 0.5)
+        # class_token and dist_token are kept unchanged
+        if orig_size != new_size:
+            print("Position interpolate from %dx%d to %dx%d" % (orig_size, orig_size, new_size, new_size))
+            extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
+            # only the position tokens are interpolated
+            pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
+            pos_tokens = pos_tokens.reshape(-1, orig_size, orig_size, embedding_size).permute(0, 3, 1, 2)
+            pos_tokens = torch.nn.functional.interpolate(
+                pos_tokens, size=(new_size, new_size), mode='bicubic', align_corners=False)
+            pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
+            new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
+            checkpoint_model['pos_embed'] = new_pos_embed
